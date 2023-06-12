cbc3771e66b36336e0e2f8068177f82a7c128ae4
Eddie Kao <eddie@5xcampus.com>
2023-06-12 18:16:06 +0800

    範例下載

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  // 程式碼寫這裡
+
+  return (
+    <div className="App">
+      <h1>貪食蛇</h1>
+      <div className="game-area"></div>
+    </div>
+  );
+}
+
+export default App;
