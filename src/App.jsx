dae1e7b783aa9065c8e4752805f468ae853e496b
Chun-Hsiang Yu <kyle.yu@tpisoftware.com>
2025-01-10 14:38:44 +0800

    add game-area
    - 加入遊戲區域
    - 將遊戲區域的邊長由20改為15

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,17 @@ function App() {
   return (
     <div className="App">
       <h1>貪食蛇</h1>
-      <div className="game-area"></div>
+      <div className="game-area">
+        {Array(15) /*每一列全部有幾格*/
+          .fill(0)
+          .map((_, rowY) =>
+            Array(15) /*有幾列*/
+              .fill(0)
+              .map((_, colX) => {
+                return <div className="cell" key={`${colX}-${rowY}`}></div>;
+              })
+          )}
+      </div>
     </div>
   );
 }
