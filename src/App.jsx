fca23e9d5d244d8afb89a9a395e85ecd6f423f36
Chun-Hsiang Yu <kyle.yu@tpisoftware.com>
2025-01-10 15:05:16 +0800

    show snake's body and apple
    - 設定變數-蛇身(陣列)
    - 設定變數-蘋果(目標物)
    - 抓取變數的位置
    - 調整className，讓css抓到

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
   // 程式碼寫這裡
-
+  const [snake, setSnake] = useState([
+    { x: 5, y: 5 },
+    { x: 5, y: 6 },
+    { x: 5, y: 7 },
+  ]); // 蛇的起始位置
+  const [apple, setApple] = useState({ x: 6, y: 6 }); // 蘋果的位置
   return (
     <div className="App">
       <h1>貪食蛇</h1>
@@ -13,7 +19,20 @@ function App() {
             Array(15) /*有幾列*/
               .fill(0)
               .map((_, colX) => {
-                return <div className="cell" key={`${colX}-${rowY}`}></div>;
+                let celltype = "";
+                if (apple.x === colX && apple.y === rowY) {
+                  celltype = "apple";
+                }
+                if (snake.some((body) => body.x === colX && body.y === rowY)) {
+                  celltype = "snake";
+                }
+
+                return (
+                  <div
+                    className={`cell ${celltype}`}
+                    key={`${colX}-${rowY}`}
+                  ></div>
+                );
               })
           )}
       </div>
